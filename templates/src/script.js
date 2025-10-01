393956c6d4d08fc656f091649afdea015a8b41bf
DaniDev <danieldavidsh8@gmail.com>
2025-10-01 12:32:47 -0400

    commit 4

--- templates/src/script.js
+++ templates/src/script.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const fechaAcceso = new Date();
-    
-    // Formatear datos
-    const datos = {
-        fecha: fechaAcceso.toISOString().split('T')[0],
-        hora: fechaAcceso.toTimeString().split(' ')[0],
-        dia_semana: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'][fechaAcceso.getDay()],
-        timestamp: fechaAcceso.getTime()
-    };
+    // 1. Código del sidebar
+    const menuToggle = document.querySelector('.menu-toggle');
+    const sidebar = document.querySelector('.sidebar');
+    const closeBtn = document.querySelector('.close-btn');
+    const overlay = document.querySelector('.sidebar-overlay');
 
-    // Configurar petición
-    fetch('http://localhost:5000/registrar-acceso', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datos)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor:', data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    menuToggle.addEventListener('click', () => {
+        sidebar.classList.add('active');
+        overlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
     });
+
+    closeBtn.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    });
+
+    overlay.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    });
+
+    // 2. Animación de fondo
+    const backgroundBox = document.querySelector('.hero');
+    const images = [
+        'src/assets/img/Fondo/img-1.jpeg',
+        'src/assets/img/Fondo/img-2.jpeg',
+    ];
+
+    
+
+    let currentIndex = 0;
+
+    function changeBackground() {
+        currentIndex = (currentIndex + 1) % images.length;
+        backgroundBox.style.backgroundImage = 
+            `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('${images[currentIndex]}')`;
+    }
+
+    // Iniciar animación
+    setInterval(changeBackground, 5000);
+
+    console.log(changeBackground);
 });
