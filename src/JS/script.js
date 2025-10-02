b5d149d826608c40843f65b3040f512b33d7bb86
DaniDev <danieldavidsh8@gmail.com>
2025-10-02 13:30:26 -0400

    commit 5

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // 1. Código del sidebar
+    /* ============================================= CÓDIGO PARA EL CONTROL DEL SIDEBAR (MENÚ LATERAL)=============================================*/
+
+
     const menuToggle = document.querySelector('.menu-toggle');
     const sidebar = document.querySelector('.sidebar');
     const closeBtn = document.querySelector('.close-btn');
     const overlay = document.querySelector('.sidebar-overlay');
+
+    /* ============================================= EVENTO: ABRIR SIDEBAR ============================================= */
 
     menuToggle.addEventListener('click', () => {
         sidebar.classList.add('active');
@@ -11,37 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = 'hidden';
     });
 
+    /* =============================================  EVENTO: CERRAR SIDEBAR (BOTÓN DE CIERRE)============================================= */
     closeBtn.addEventListener('click', () => {
         sidebar.classList.remove('active');
         overlay.classList.remove('active');
         document.body.style.overflow = 'auto';
     });
 
+    /* =============================================   EVENTO: CERRAR SIDEBAR (CLIC EN OVERLAY)============================================= */
     overlay.addEventListener('click', () => {
         sidebar.classList.remove('active');
         overlay.classList.remove('active');
         document.body.style.overflow = 'auto';
     });
-
-    // 2. Animación de fondo
-    const backgroundBox = document.querySelector('.hero');
-    const images = [
-        'src/assets/img/Fondo/img-1.jpeg',
-        'src/assets/img/Fondo/img-2.jpeg',
-    ];
-
-    
-
-    let currentIndex = 0;
-
-    function changeBackground() {
-        currentIndex = (currentIndex + 1) % images.length;
-        backgroundBox.style.backgroundImage = 
-            `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('${images[currentIndex]}')`;
-    }
-
-    // Iniciar animación
-    setInterval(changeBackground, 5000);
-
-    console.log(changeBackground);
 });
